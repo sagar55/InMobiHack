734e5c90208e98750221c9fbc23e37b3b0ee31a0
Gandhi <gansagar@10ddb1f0b560.ant.amazon.com>
2016-02-12 03:00:56 +0530

    update1

--- restTwt.py
+++ restTwt.py
@@ -1,12 +1,17 @@
 import tweepy
 import json
+import ConfigParser
 from tweepy import OAuthHandler
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
 
+parser = ConfigParser.ConfigParser()
+parser.read('key_tokens.config')
+CONSUMER_KEY = parser.get('critical_information', 'CONSUMER_KEY')
+CONSUMER_SECRET = parser.get('critical_information', 'CONSUMER_SECRET')
+ACCESS_TOKEN = parser.get('critical_information', 'ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = parser.get('critical_information', 'ACCESS_TOKEN_SECRET')
+
+#2208027565
 def do_auth():
 	auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
@@ -27,8 +32,7 @@ def test(api):
 
 		# way of getting first 10 tweets text appearing in our home feed 
 		for tweet in tweepy.Cursor(api.home_timeline).items(10):
-    		print tweet.text
-
+			print tweet.text
 
     	#gettng all the followers 
 		for friend in tweepy.Cursor(api.friends).items():
@@ -41,9 +45,37 @@ def test(api):
 		#getting all the tweets a user has tweets (if user id not provided then get tweet by defaul of registered api key account/authorised acc)
 		for tweet in tweepy.Cursor(api.user_timeline).items():
 			f.write(json.dumps(tweet._json,indent=4)+"\n\n\n\n\n")
-	
+
+		for status in tweepy.Cursor(api.user_timeline,id="505452845").items():
+			f.write(json.dumps(status._json,indent=4)+"\n\n\n\n\n")
+
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+def test2(api):
+	with open('mytweets.json', 'w') as f:
+		cnt=1
+		# for page in tweepy.Cursor(api.user_timeline).pages():
+		# 	f.write(str(cnt))
+		# 	cnt=cnt+1
+		# 	for status in page:
+		# 		f.write(json.dumps(status._json,indent=4)+"\n\n\n\n\n")
+		# 	f.write("#########################")
+
+		# f.write("###########################################")
+		# for status in tweepy.Cursor(api.user_timeline,id="134758540").items():
+		# 	f.write(json.dumps(status._json,indent=4)+"\n\n\n\n\n")
+
+		user = api.me()
+		print user.name
+
+
+
 
 api = do_auth()
-test(api)
-
-
+test2(api)
