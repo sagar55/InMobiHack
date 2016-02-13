650b9ccdd7fceaf8e029b879716ee48970442716
Gandhi <gansagar@10ddb1f0b560.ant.amazon.com>
2016-02-13 18:15:47 +0530

    improving streaming of tweets

--- streamTwt.py
+++ streamTwt.py
@@ -30,12 +30,12 @@ class MyListener(StreamListener):
         self.start = time.time()
         self.current = self.start
         self.count = 0
-        self.tweet_limit = 5
-        self.time_limit = 5*60 # 5 minute
+        self.tweet_limit = 10
+        self.time_limit = 24*60*60 # 24 hour
         self.file_name = get_file_name()
 
     def on_data(self, data):
-        #json_data = json.loads(data)
+        parsed_data = json.loads(data)
         #self.current = time.time()
         #elapse_time = self.current - self.start
         
@@ -52,8 +52,8 @@ class MyListener(StreamListener):
             self.file_name = file_name
         try:
             with open(self.file_name, 'a') as f:
-                f.write(data)
-                #f.write(json.dumps(json_data,indent=4)+"\n\n\n\n")
+                #f.write(data+"\n")
+                f.write(json.dumps(parsed_data,indent=4)+"\n\n\n\n")
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -80,14 +80,19 @@ def do_auth():
     auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
     return auth
 
-def test():
-    return 0
+def fetch_tweet(userid):
+    if not userid:
+        stream.userstream()
+    else:
+        stream.sitestream(follow=[userid])
+
 
 if __name__ == '__main__':
     listener = MyListener()
     stream = Stream(do_auth(), listener)
     #stream.filter(follow=[#id], track=['#python']) #getting all tweet of following id having hashtag as "python"
     #stream.filter(track=['#python'])
-    stream.userstream()
+    fetch_tweet("")
+    #stream.userstream()
 
 
